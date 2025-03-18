39094aac51a93b3edb38554be30321f6a79b402a
Yagho Petini <yaghojp@gmail.com>
2025-03-17 23:24:55 -0300

    AULA [46] - Apply Floor Scroll

---
+++ ground/ground.gd
@@ -0,0 +1,36 @@
+extends Node2D
+class_name Ground
+
+signal  onGroundPlayerCrash
+
+@export_category("Variables")
+@export var _speed: int = -150
+
+@onready var ground_1: Area2D = $Ground1
+@onready var ground_2: Area2D = $Ground2
+@onready var sprite_2d: Sprite2D = $Ground1/Sprite2D
+@onready var sprite_2d2: Sprite2D = $Ground2/Sprite2D
+
+var _width: int
+
+func _ready() -> void:
+	_width = sprite_2d.texture.get_width()
+
+
+func _process(delta: float) -> void:
+	ground_1.global_position.x += _speed * delta
+	ground_2.global_position.x += _speed * delta
+	
+	
+	if ground_1.global_position.x < -_width / 2:
+		ground_1.global_position.x = ground_2.global_position.x + _width
+	if ground_2.global_position.x < -_width / 2:
+		ground_2.global_position.x = ground_1.global_position.x + _width
+
+
+func _on_ground_body_entered(body: Node2D) -> void:
+	onGroundPlayerCrash.emit()
+	_speed = 0
+	var _playerRef = body as Player
+	_playerRef.stopMovement()
+	_playerRef.stopGravity()
--- worlds/world.gd
+++ worlds/world.gd
@@ -9,3 +9,7 @@ func _on_player_on_game_started() -> void:
 
 func _on_spawner_on_obstacle_crash() -> void:
 	player.stopMovement()
+
+
+func _on_ground_on_ground_player_crash() -> void:
+	spawner._stopObstacles()
