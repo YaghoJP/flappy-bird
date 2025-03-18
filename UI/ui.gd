61e9b3aaf3a91d8924a45fc7ecf4d868c9f4681b
Yagho Petini <yaghojp@gmail.com>
2025-03-18 17:16:45 -0300

    AULA [49] - Save Score

--- UI/ui.gd
+++ UI/ui.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 class_name UI
 
+const MEDAL_BRONZE = preload("res://UI/medalBronze.png")
+const MEDAL_GOLD = preload("res://UI/medalGold.png")
+const MEDAL_SILVER = preload("res://UI/medalSilver.png")
+
 @onready var scoreLabel: Label = %ScoreLabel
 @onready var startMenu: Control = $MarginContainer/StartMenu
 @onready var gameOverContainer: VBoxContainer = %GameOver
+@onready var medalImage: TextureRect = %MedalImage
+@onready var currentScore: Label = %CurrentScore
+@onready var highScore: Label = %HighScore
 
 func _ready() -> void:
 	scoreLabel.text = "0"
@@ -17,3 +24,14 @@ func gameOver() -> void:
 
 func _on_ok_button_pressed() -> void:
 	get_tree().reload_current_scene()
+
+func calculateScore(amount: int, high: int) -> void:
+	currentScore.text = str(amount)
+	if amount >= 20:
+		medalImage.texture = MEDAL_GOLD
+	elif amount >= 10:
+		medalImage.texture = MEDAL_SILVER
+	else:
+		medalImage.texture = MEDAL_BRONZE
+		
+	highScore.text = str(high)
--- worlds/world.gd
+++ worlds/world.gd
@@ -1,10 +1,29 @@
 extends Node2D
 
+const SAVE_FILE: String = "user://score.save"
+
 @onready var spawner: Spawner = $Spawner
 @onready var player: Player = $Player
 @onready var ui: UI = $UI
 
 var _score: int = 0
+var _highScore: int = 0
+
+func _ready() -> void:
+	_loadHighScore()
+
+
+func _loadHighScore() -> void:
+	var file: FileAccess = FileAccess.open(SAVE_FILE, FileAccess.READ)
+	
+	if file:
+		_highScore = file.get_32()
+
+func _saveScore() -> void:
+	if _score > _highScore:
+		_highScore = _score
+		var file: FileAccess = FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+		file.store_32(_highScore)
 
 func _on_player_on_game_started() -> void:
 	spawner.timer.start()
@@ -15,8 +34,11 @@ func _on_spawner_on_obstacle_crash() -> void:
 
 func _on_ground_on_ground_player_crash() -> void:
 	spawner._stopObstacles()
+	_loadHighScore()
 	ui.gameOver()
+	ui.calculateScore(_score, _highScore)
 
 func _on_spawner_player_scored() -> void:
 	_score += 1
 	ui.updateScore(_score)
+	_saveScore()
